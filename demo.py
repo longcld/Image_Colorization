de022ecc1cca7ebd92262ed9163e535ce2a22ed5
Long Lịch <38462352+longcld@users.noreply.github.com>
2019-11-06 01:09:34 +0700

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 # import the necessary packages
 import os
 import random
-
+import argparse
 import cv2 as cv
 import keras.backend as K
 import numpy as np
@@ -11,109 +11,100 @@ from config import img_rows, img_cols
 from config import nb_neighbors, T, epsilon
 from model import build_model
 
-if __name__ == '__main__':
-    channel = 3
+def parser_arg():
+    parser = argparse.ArgumentParser(add_help=True)
+    parser.add_argument('--save', '-s', default="test", type=str, help="Folder to save test image prediction")
+    parser.add_argument('--image', '-i', default=None, type=str, help="Path to test image")
+    return parser.parse_args()
 
-    model_weights_path = 'models/model.00-10.1351.hdf5'
+if __name__ == '__main__':
+    arg = parser_arg()
+    
+    channel = 3
+    
+    model_weights_path = 'models/model.06-2.5489.hdf5'
+
     model = build_model()
     model.load_weights(model_weights_path)
 
-    print(model.summary())
+#     print(model.summary())
+    save_folder = arg.save
+#     names_file = 'v_n.txt'
 
-    image_folder = '../Image/resized'
-    names_file = 'valid_names.txt'
-    save_folder = 'test_1'
-    with open(names_file, 'r') as f:
-        names = f.read().splitlines()
+#     with open(names_file, 'r') as f:
+#         names = f.read().splitlines()
 
-    samples = random.sample(names, 10)
+#     samples = random.sample(names, 1)
 
+#    samples = os.listdir(image_folder)
+
+#     samples.append(image_name)
     h, w = img_rows // 4, img_cols // 4
 
-    # Load the array of quantized ab value
     q_ab = np.load("data/pts_in_hull.npy")
     nb_q = q_ab.shape[0]
 
-    # Fit a NN to q_ab
     nn_finder = nn.NearestNeighbors(n_neighbors=nb_neighbors, algorithm='ball_tree').fit(q_ab)
 
-    for i in range(len(samples)):
-        image_name = samples[i]
-        filename = os.path.join(image_folder, image_name)
-        print('Start processing image: {}'.format(filename))
-        # b: 0 <=b<=255, g: 0 <=g<=255, r: 0 <=r<=255.
-        bgr = cv.imread(filename)
-        gray = cv.imread(filename, 0)
-        bgr = cv.resize(bgr, (img_rows, img_cols), cv.INTER_CUBIC)
-        gray = cv.resize(gray, (img_rows, img_cols), cv.INTER_CUBIC)
-        # L: 0 <=L<= 255, a: 42 <=a<= 226, b: 20 <=b<= 223.
-        lab = cv.cvtColor(bgr, cv.COLOR_BGR2LAB)
-        L = lab[:, :, 0]
-        a = lab[:, :, 1]
-        b = lab[:, :, 2]
-        # print('np.max(L): ' + str(np.max(L)))
-        # print('np.min(L): ' + str(np.min(L)))
-        # print('np.max(a): ' + str(np.max(a)))
-        # print('np.min(a): ' + str(np.min(a)))
-        # print('np.max(b): ' + str(np.max(b)))
-        # print('np.min(b): ' + str(np.min(b)))
-        x_test = np.empty((1, img_rows, img_cols, 1), dtype=np.float32)
-        x_test[0, :, :, 0] = gray / 255.
+    filename = arg.image
+    if len(arg.image.split('/')):
+        image_name = arg.image.split('/')[-1]
+    else:
+        image_name = arg.image
+#     filename = os.path.join(image_folder, image_name)
+    print('Start processing image: {}'.format(filename))
 
-        # L: 0 <=L<= 255, a: 42 <=a<= 226, b: 20 <=b<= 223.
-        X_colorized = model.predict(x_test)
-        X_colorized = X_colorized.reshape((h * w, nb_q))
+    bgr = cv.imread(filename)
+    gray = cv.imread(filename, 0)
+    bgr = cv.resize(bgr, (img_rows, img_cols), cv.INTER_CUBIC)
+    gray = cv.resize(gray, (img_rows, img_cols), cv.INTER_CUBIC)
 
-        # Reweight probas
-        X_colorized = np.exp(np.log(X_colorized + epsilon) / T)
-        X_colorized = X_colorized / np.sum(X_colorized, 1)[:, np.newaxis]
+    lab = cv.cvtColor(bgr, cv.COLOR_BGR2LAB)
+    L = lab[:, :, 0]
+    a = lab[:, :, 1]
+    b = lab[:, :, 2]
 
-        # Reweighted
-        q_a = q_ab[:, 0].reshape((1, 313))
-        q_b = q_ab[:, 1].reshape((1, 313))
+    x_test = np.empty((1, img_rows, img_cols, 1), dtype=np.float32)
+    x_test[0, :, :, 0] = gray / 255.
 
-        X_a = np.sum(X_colorized * q_a, 1).reshape((h, w))
-        X_b = np.sum(X_colorized * q_b, 1).reshape((h, w))
-        # print('np.max(X_a): ' + str(np.max(X_a)))
-        # print('np.min(X_a): ' + str(np.min(X_a)))
-        # print('np.max(X_b): ' + str(np.max(X_b)))
-        # print('np.min(X_b): ' + str(np.min(X_b)))
-        X_a = cv.resize(X_a, (img_rows, img_cols), cv.INTER_CUBIC)
-        X_b = cv.resize(X_b, (img_rows, img_cols), cv.INTER_CUBIC)
+    X_colorized = model.predict(x_test)
+    X_colorized = X_colorized.reshape((h * w, nb_q))
 
-        # Before: -90 <=a<= 100, -110 <=b<= 110
-        # After: 38 <=a<= 228, 18 <=b<= 238
-        X_a = X_a + 128
-        X_b = X_b + 128
-        # print('np.max(X_a): ' + str(np.max(X_a)))
-        # print('np.min(X_a): ' + str(np.min(X_a)))
-        # print('np.max(X_b): ' + str(np.max(X_b)))
-        # print('np.min(X_b): ' + str(np.min(X_b)))
+    X_colorized = np.exp(np.log(X_colorized + epsilon) / T)
+    X_colorized = X_colorized / np.sum(X_colorized, 1)[:, np.newaxis]
 
-        out_lab = np.zeros((img_rows, img_cols, 3), dtype=np.int32)
-        out_lab[:, :, 0] = lab[:, :, 0]
-        out_lab[:, :, 1] = X_a
-        out_lab[:, :, 2] = X_b
-        out_L = out_lab[:, :, 0]
-        out_a = out_lab[:, :, 1]
-        out_b = out_lab[:, :, 2]
-        # print('np.max(out_L): ' + str(np.max(out_L)))
-        # print('np.min(out_L): ' + str(np.min(out_L)))
-        # print('np.max(out_a): ' + str(np.max(out_a)))
-        # print('np.min(out_a): ' + str(np.min(out_a)))
-        # print('np.max(out_b): ' + str(np.max(out_b)))
-        # print('np.min(out_b): ' + str(np.min(out_b)))
-        out_lab = out_lab.astype(np.uint8)
-        out_bgr = cv.cvtColor(out_lab, cv.COLOR_LAB2BGR)
-        # print('np.max(out_bgr): ' + str(np.max(out_bgr)))
-        # print('np.min(out_bgr): ' + str(np.min(out_bgr)))
-        out_bgr = out_bgr.astype(np.uint8)
+    # Reweighted
+    q_a = q_ab[:, 0].reshape((1, 313))
+    q_b = q_ab[:, 1].reshape((1, 313))
 
-        if not os.path.exists(save_folder):
-            os.makedirs(save_folder)
+    X_a = np.sum(X_colorized * q_a, 1).reshape((h, w))
+    X_b = np.sum(X_colorized * q_b, 1).reshape((h, w))
 
-        cv.imwrite(save_folder + '/{}_image.png'.format(i), gray)
-        cv.imwrite(save_folder + '/{}_gt.png'.format(i), bgr)
-        cv.imwrite(save_folder + '/{}_out.png'.format(i), out_bgr)
+    X_a = cv.resize(X_a, (img_rows, img_cols), cv.INTER_CUBIC)
+    X_b = cv.resize(X_b, (img_rows, img_cols), cv.INTER_CUBIC)
+
+    X_a = X_a + 128
+    X_b = X_b + 128
+
+
+    out_lab = np.zeros((img_rows, img_cols, 3), dtype=np.int32)
+    out_lab[:, :, 0] = lab[:, :, 0]
+    out_lab[:, :, 1] = X_a
+    out_lab[:, :, 2] = X_b
+    out_L = out_lab[:, :, 0]
+    out_a = out_lab[:, :, 1]
+    out_b = out_lab[:, :, 2]
+    out_lab = out_lab.astype(np.uint8)
+    out_bgr = cv.cvtColor(out_lab, cv.COLOR_LAB2BGR)
+    out_bgr = out_bgr.astype(np.uint8)
+
+    if not os.path.exists(save_folder):
+        os.makedirs(save_folder)
+
+    cv.imwrite(save_folder + '/{}_image.png'.format(image_name), gray)
+   # cv.imwrite(save_folder + '/{}_gt.png'.format(image_name), bgr)
+    cv.imwrite(save_folder + '/{}_out.png'.format(image_name), out_bgr)
+
+    print("Done")
 
     K.clear_session()
